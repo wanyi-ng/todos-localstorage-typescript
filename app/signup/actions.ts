83e474d8e5efc482361c6275c6af62c2959b66fc
Wanyi Ng <wanyi.ng1975@gmail.com>
2024-06-30 15:12:51 -0400

    signup actions

---
+++ app/signup/actions.ts
@@ -0,0 +1,31 @@
+'use server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export async function signup(formData: FormData) {
+  const supabase = createClient()
+
+  const data = {
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
+    options: {
+      data: {
+        first_name: formData.get('firstName') as string,
+        last_name: formData.get('lastName') as string,
+      }
+    }
+  }
+
+  console.log("data: ", data)
+
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.log(error)
+
+    redirect('/error')
+  }
+  revalidatePath('/', 'layout')
+  redirect('/login')
+}
